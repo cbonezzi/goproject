d6207e3c4a1d971728a9b2dff06b1353ceaf3661
cesar.bonezzi <cesar.bonezzi@gmail.com>
2019-05-31 00:45:52 -0500

    learn how to use logging separation learn about storing flags in stringvar

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,14 +4,31 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+//Config struct
+type Config struct{
+	Addr string
+	StaticDir string
+}
 
 func main(){
 
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address1")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static","Path to static assests")
+
 	//flag name = addr, default value = :4000, description of flag = HHTP...
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	//addr := flag.String("addr", ":4000", "HTTP network address")
+	//isProd := flag.Bool("IsProd", true, "Flag to determine if prod")
 
 	flag.Parse()
+
+	//way of creating log distinction and separation...perhaps put this into a class,
+	//and abstract it out, to use it globally?
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	//use the http.newservemux() function to initialize a new servemux,
 	//then register the home function as the handler for the "/" URL pattern.
@@ -25,7 +42,8 @@ func main(){
 	//static url for handles
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on ", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", cfg.Addr)
+	log.Println("Is Production", &cfg.StaticDir)
+	err := http.ListenAndServe(cfg.Addr, mux)
+	errorLog.Fatal(err)
 }
